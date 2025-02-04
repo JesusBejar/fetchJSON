99495cac1780725e42fcea3568ab4a7d4acd1e24
Jesus Bejar <israelbejarii@gmail.com>
2025-02-04 09:58:11 -0600

    posted a post

---
+++ POST/scripts.js
@@ -0,0 +1,29 @@
+const URLurl = "https://jsonplaceholder.typicode.com/posts"
+// button elements
+const btn = document.querySelector("#button-one")
+// console.log('one')
+
+// buttons that fire functions
+btn.addEventListener("click", sendAPost)
+// console.log('two')
+
+// gets titles of all posts
+async function sendAPost() {
+    try {
+        console.log('clicked')
+        const response = await fetch(URLurl , {
+            method: 'POST', 
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: {
+                title: "post post post"
+            }
+        })
+        const post = await response.json()
+        console.log(post)
+    } catch (e) {
+        response.error(e)
+    }
+
+}
