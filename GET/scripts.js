044038dd12499345d6c2a1d11c08752b4f8ff903
Jesus Bejar <israelbejarii@gmail.com>
2025-02-04 10:04:35 -0600

    you can only use fetch to GET & POST?

--- GET/scripts.js
+++ GET/scripts.js
@@ -22,6 +22,7 @@ async function getAllPosts() {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
         const json = await response.json()
         const postTitle = await json.map(postTitle => postTitle.title)
+        console.log(postTitle)
     } catch (e) {
         response.error(e)
     }
