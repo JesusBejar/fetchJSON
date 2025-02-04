a77f2f5779f4010023dc07d92ae6df69bb6c29cc
Jesus Bejar <israelbejarii@gmail.com>
2025-02-04 09:20:21 -0600

    added try-catch

--- GET/scripts.js
+++ GET/scripts.js
@@ -15,9 +15,14 @@ btn.addEventListener("click", getAllPosts)
 
 // gets titles of all posts
 async function getAllPosts() {
-    console.log('clicked')
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const json = await response.json()
-    const postTitle = await json.map(postTitle => postTitle.title)
+    try {
+        console.log('clicked')
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const json = await response.json()
+        const postTitle = await json.map(postTitle => postTitle.title)
+    } catch (e) {
+        response.error(e)
+    }
+
 }
 // console.log('three')
