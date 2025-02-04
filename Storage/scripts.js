5c361a134c88d208524b0d09293a76453241fd64
Jesus Bejar <israelbejarii@gmail.com>
2025-02-04 14:55:29 -0600

    updated items

--- Storage/scripts.js
+++ Storage/scripts.js
@@ -1,6 +1,8 @@
 // Local storage 10MB
 localStorage.setItem('Name', 'Yo Momma')
+localStorage.setItem('Name', 'Joe Momma')
 // Cookies 4KB
 
 // Session storage 5MB
 sessionStorage.setItem("Age", "24")
+sessionStorage.setItem("Age", "42")
