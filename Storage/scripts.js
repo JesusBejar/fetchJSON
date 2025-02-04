faad7b8c53c5992f9c537b50cf2b9bd736b97243
Jesus Bejar <israelbejarii@gmail.com>
2025-02-04 14:56:06 -0600

    removed item

--- Storage/scripts.js
+++ Storage/scripts.js
@@ -1,8 +1,9 @@
 // Local storage 10MB
-localStorage.setItem('Name', 'Yo Momma')
+// localStorage.setItem('Name', 'Yo Momma')
 localStorage.setItem('Name', 'Joe Momma')
+sessionStorage.removeItem("Name")
 // Cookies 4KB
 
 // Session storage 5MB
-sessionStorage.setItem("Age", "24")
+// sessionStorage.setItem("Age", "24")
 sessionStorage.setItem("Age", "42")
